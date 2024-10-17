3489c30c871799f112ace30b3a492ab93a283735
Shukhratbek Mamadaliev <shukhratbek_mamadaliev@epam.com>
2024-10-18 03:10:11 +0500

    add task02

---
+++ task02/app/lambdas/hello_world/index.js
@@ -0,0 +1,16 @@
+exports.handler = async (event) => {
+    const path = event.rawPath;
+    const method = event.requestContext.http.method;
+
+    if (path === "/hello" && method === "GET") {
+        return {
+            statusCode: 200,
+            message: "Hello from Lambda"
+        };
+    } else {
+        return {
+            statusCode: 400,
+            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
+        };
+    }
+};
